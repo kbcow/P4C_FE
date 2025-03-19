0c06b09aef82e74f5c57b2ba4f3bd11267606db3
AD1065 <ad1065@AD1065ui-MacBookPro.local>
2025-03-19 22:11:01 +0900

    npm 설치

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
-// import React from "react";
+import React from "react";
 
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
+function App() {
+  return <h1>ds</h1>;
+}
 
-// export default App;
+export default App;
